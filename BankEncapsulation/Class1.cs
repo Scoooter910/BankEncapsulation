37faca43e1e8e4e4db04ed75ccc8cb5eb1eb64cc
Michele Mostoller <MMostoller@att.net>
2024-06-12 14:49:10 -0400

    Exercise

---
+++ BankEncapsulation/Class1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulation
+{
+    public class BankAccount
+{
+        private double _deposit = 0;
+        private double _withdraw = 0;
+        private double _balance = 0;
+       
+
+        public void Deposit(double money)
+
+        {
+            _balance = +money;
+        
+        }
+        public double GetBalance()
+        {
+            Console.WriteLine($"{_balance}");
+            return _balance;
+        
+        }
+        public void Withdraw(double amount)
+        {
+            _balance -= amount;
+        }
+
+        public void Withdraw()
+        {
+            Console.WriteLine($"{_withdraw}");
+        
+        }
+        
+    }
+
+
+
+}
--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -2,9 +2,44 @@
 {
     public class Program
     {
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            BankAccount account1 = new BankAccount();
+            account1.Deposit(5000);
+            account1.GetBalance();
+            account1.Withdraw(2000);
+            account1.GetBalance();
+            Console.WriteLine("Welcome, Please enter an option:");
+            Console.Write("Deposit");
+            Console.WriteLine();
+            Console.Write("Withdraw");
+            Console.WriteLine();
+            Console.Write("Get Balance");
+            Console.WriteLine();
+
+            string answer = Console.ReadLine();
+            if (answer == "Deposit")
+
+            {
+                Console.WriteLine("How much would you like to deposit?");
+                double depositAmount = double.Parse(Console.ReadLine());
+                account1.Deposit(depositAmount);
+            }
+            else if (answer == "Withdraw")
+            {
+                Console.WriteLine("How much would you like to withdraw?");
+                double withdrawAmount = double.Parse(Console.ReadLine());
+                account1.Withdraw(withdrawAmount);
+
+            }
+            else if (answer == "Get Balance")
+            {
+
+                Console.WriteLine("Balance");
+                var amount = account1.GetBalance();
+                Console.WriteLine($"Your balance is:{amount}");
+            }
         }
     }
 }
